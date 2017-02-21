962a8d7c5c34f6a762adf22b1143fc00a248e0ce
Sai Samarth R Phaye <2014csb1029@iitrpr.ac.in>
2017-02-21 16:05:05 +0530

    updating the frame js

--- js/frame.js
+++ js/frame.js
@@ -1,8 +1,10 @@
 function setScreenSize()
 {
 	var size = screen.width;
-	size = size - 50;
+	size = size - 125;
 	document.getElementById("container").style.width = size+"px";
+	document.getElementById("container").style.marginLeft = "auto";
+	document.getElementById("container").style.marginRight = "auto";
 	document.getElementById("container").style.opacity = "1";
 
 }

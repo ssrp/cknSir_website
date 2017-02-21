34ffd41d8ca11813efefeffd9c901b959c716c3c
Sai Samarth R Phaye <2014csb1029@iitrpr.ac.in>
2017-02-21 14:46:57 +0530

    updating index

--- index.php
+++ index.php
@@ -7,39 +7,41 @@
 	<!--
 		Importing the css files.
 		-->
+	<link href = "css/bootstrap.min.css" rel = "stylesheet" type = "text/css" />
+	<link href = "css/dataTables.bootstrap.min.css" rel = "stylesheet" type = "text/css" />
+	<link href = "css/fixedHeader.bootstrap.min.css" rel = "stylesheet" type = "text/css" />
+	<link href = "css/responsive.bootstrap.min.css" rel = "stylesheet" type = "text/css" />
 	<link href = "css/frame.css" rel = "stylesheet" type = "text/css" />
 	<link href = "css/index.css" rel = "stylesheet" type = "text/css" />
-	<link href = "css/bootstrap.min.css" rel = "stylesheet" type = "text/css" />
 
 	<!--
 		Setting The Logo!
 		-->
-	<link rel="shortcut icon" href="images/logo.jpg">
+	<link rel="shortcut icon" href="images/icon.png">
 </head>
 <body>
 	<center>
 		<div id = "container">
-			<h1 style = "font-family:'Varela'; padding:0em; margin-bottom:0em"><a href = "http://www.iitrpr.ac.in" style = "color:black" target = "_blank">Indian Institute of Technology Ropar</a></h1>
-			<br><h3 style = "font-family:'Varela'; margin:0em; padding:0em">NARAYANAN C KRISHNAN</h3>
-			
-			<div id = "navbar">
+			<div style = "height:8em; width:75%; border:2px solid transparent; margin-top:0.2em; margin-bottom:-1em">
+				
+				<div id = "top_data" style= "display:inline-block; float: left;">
+						<h1 style = "font-family:'Varela'; padding:0em; margin-bottom:0em; padding-left:10px; padding-right:10px;"><a href = "http://www.iitrpr.ac.in" style = "color:black" target = "_blank">Indian Institute of Technology Ropar</a></h1>
+					<br><h3 style = "font-family:'Varela';float:left; margin:0em; padding:0em; padding-left:10px;display:inline-block; padding-right:10px">NARAYANAN C KRISHNAN</h3>
+				</div>
+				<img src = "images/logo.jpg" style = "float:right;height:100%; transform:translateY(0.5em)" />
+			</div>
+			<div id = "navbar" style = "align: center">
 				<br>
 				<div id = "navbar_hr">
 					<hr width = "75%"/>
-					<hr width = "75%" style = "margin-top:-1.4em; padding:0em"/>
-					<hr width = "75%" style = "margin-top:-1.5em; padding:0em"/>
-					<hr width = "75%" style = "margin-top:-1.5em; padding:0em"/>
-					<hr width = "75%" style = "margin-top:-1.5em; padding:0em"/>
-					<hr width = "75%" style = "margin-top:-1.5em; padding:0em"/>
-					<hr width = "75%" style = "margin-top:-1.5em; padding:0em"/>
 				</div>
-				<div id = "navbar_options">
+				<div id = "navbar_options" style= "font-size:1.1em">
 						<ul class = "toolbar">
-						  <li><a class="active" href="./index.php">HOME</a></li>
-						  <li><a href="./research.php">RESEARCH</a></li>
-						  <li><a href="./publications.php">PUBLICATIONS</a></li>
-						  <li><a href="./teaching.php">TEACHING</a></li>
-						  <li><a href="./resources.php">RESOURCES</a></li>
+						  <li><a class="active" href="./index.php" style= "color:#0000A0">HOME</a></li>
+						  <li><a href="./research.php" style= "color:#0000A0">RESEARCH</a></li>
+						  <li><a href="./publications.php" style= "color:#0000A0">PUBLICATIONS</a></li>
+						  <li><a href="./teaching.php" style= "color:#0000A0">TEACHING</a></li>
+						  <li><a href="./resources.php" style= "color:#0000A0">RESOURCES</a></li>
 						</ul>
 				</div>
 			</div>
@@ -54,27 +56,122 @@
 						<h2>Narayanan C Krishnan</h2>
 						<h3>Assistant Professor</h3>
 						<h3><a href="http://www.iitrpr.ac.in/cse">Department of Computer Science and Engineering</a></h3>
-						<h3>318, Academic Block</h3>
-						<h3>Indian Institute of Technology Ropar</h3>
-						<h3>Nangal Road, Rupnagar</h3>
-						<h3>Punjab 140001, India</h3>
+						<h3>318, Indian Institute of Technology Ropar</h3>
+						<h3>Rupnagar, PB</h3>
+						<h3>India 140001</h3>
 						<h3>Phone: +91 (1881) 24-2273</h3>
 						<br>
-						<h3>Curriculum Vitae: <a href="Resume/narayanan_cv.pdf" target = "_blank">Click here!</a></h3>
+						<h3><a href="Resume/narayanan_cv.pdf" target = "_blank">Curriculum Vitae</a></h3>
 						<br>
 					</div>
 				</div>
 
-				<div id = "container_data_one">
-					<center>
+				<div id = "container_data_one" style = "text-align:justify;">
+					
 						<span style = "font-family:'Garamond'">
 							I joined the <a href="http://www.iitrpr.ac.in/cse">Department of Computer Science and Engineering</a> at <a href="http://www.iitrpr.ac.in">IIT Ropar</a> in fall 2013. Prior to that I was associated with the <a href="https:/casas.ailab.eecs.wsu.edu">CASAS</a> group at Washington State University, where I worked in the area of data mining applied to activity recognition and smart home technologies. I completed my PhD in 2010 under the supervision of <a href="https://webapp4.asu.edu/directory/person/88253">Prof. Sethuraman Panchanathan</a> from Arizona State Univerity.  I like to work on some fundamental machine learning/data mining probelms that are inspired by application domains such as activity recognition, pervasive health care and recently from ICT4D.
 						</span>
-					</center>
+					
 				</div>
-				<div id = "container_recent_updates">
-					<center><h3 style = "font-family:'Varela'">- <b>RECENT UPDATES</b> -</h3></center>
+
+				<div id = "container_recent_updates" style = "text-align:justify; font-family:Garamond">
+						<center><h3 style = "font-family:'Varela';">- <b>RECENT UPDATES</b> <img src="images/new1.png" style = "vertical-align:middle" width="25" height="25" /> -</h3></center>
+					<table id="recent_updates" class="table table-striped table-bordered" style = "border:2px solid transparent; padding:0.5em" cellspacing="0" width="100%">
+				        <thead style = "display:none">
+				            <tr>
+				                <th style = "width:4em">Date&nbsp;&nbsp;</th>
+				                <th>Update</th>
+				            </tr>
+				        </thead>
+				        <tbody>
+				            <tr>
+				                <td style = "width:5em">Spring 2017</td>
+				                <td>
+				                	I will be teaching <a href = "http://cse.iitrpr.ac.in/ckn/courses/s2017/csl302/csl302.html">CSL302 - Artificial Intelligence</a>. Check the course webpage for details.
+				                </td>
+				            </tr>
+				            <tr>
+				                <td>Dec '16</td>
+				                <td>
+				                	Spot Garbage team wins the INAE Student Project of the year award for 2016. Congratulations Gaurav, Kaushal and Mohit!
+				                </td>
+				            </tr>
+				            <tr>
+				                <td>Nov '16</td>
+				                <td>
+				                	Brain Segmentation paper accepted to ICVGIP 2016. Congratulations Apoorva and Gaurav!
+				  	            </td>
+				            </tr>
+				            <tr>
+				                <td>Aug '16</td>
+				                <td>
+				                	Grateful to Microsoft for the unrestricted research grant.
+				  	            </td>
+				            </tr>
+				            <tr>
+				                <td>Fall 2016</td>
+				                <td>
+				                	I will be teaching <a href="courses/f2016/csl603/csl603.html">CSL603 - Machine Learning</a> in Fall 2016. Check the course webpage for details.
+				  	            </td>
+				            </tr>
+				            <tr>
+				                <td>June '16</td>
+				                <td>
+				                	Spot Garbage paper accepted to UbiComp 2016! Congratulations Gaurav, Kaushal and Mohit - an all undergraduate team!
+				  	            </td>
+				            </tr>
+				            <tr>
+				                <td>June '16</td>
+				                <td>
+				                	Thanks ACM IARCS, IJCAI, and Microsoft for supporting Sanatan's travel to IJCAI 2016.
+				  	            </td>
+				            </tr>
+				            <tr>
+				                <td>May '16</td>
+				                <td>
+				                	Grateful to NVIDIA for donating a TitanX card through the academic hardware grant request program.
+				  	            </td>
+				            </tr>
+				            <tr>
+				                <td>Apr '16</td>
+				                <td>
+				                	<a href="http://www.iitrpr.ac.in/2016-microsoft-imagine-cup-national-champions">Team KudaPehchano from IIT Ropar consisting of Gaurav, Kaushal, and Mohit are the 2016 Microsoft Imagine Cup National Champions in the World Citizenship category! Congratulations guys! and Good luck at the World Semifinals.</a>
+				  	            </td>
+				            </tr>
+				            <tr>
+				                <td>Apr '16</td>
+				                <td>
+				                	Sanatan's first paper accepted to IJCAI 2016!
+				  	            </td>
+				            </tr>
+				            <tr>
+				                <td>May '16</td>
+				                <td>
+				                	DST-YSS project approved for conducting research in "Activity Learning in Smart Environments"
+				  	            </td>
+				            </tr>
+				            <tr>
+				                <td>-</td>
+				                <td>
+				                	<a href="http://as.wiley.com/WileyCDA/WileyTitle/productCd-111889376X.html">Co-authored book on Activity Learning: Discovering, Recognizing, and Predicting Human Behavior from Sensor Data</a> was published by Wiley.
+				  	            </td>
+				            </tr>
+				        </tbody>
+				    </table>
+				</div>
+				<!--
+				<div id = "container_recent_updates" style = "text-align:justify">
+					<center><h3 style = "font-family:'Varela';">- <b>RECENT UPDATES</b> <img src="images/new1.png" style = "vertical-align:middle" width="25" height="25" /> -</h3></center>
 					<ul>
+						<li>I will be teaching <a href = "http://cse.iitrpr.ac.in/ckn/courses/s2017/csl302/csl302.html">CSL302 - Artificial Intelligence</a> in Spring 2017. Check the course webpage for details.</li>
+						
+						<li>
+							Dec '16 Spot Garbage team wins the INAE Student Project of the year award for 2016. Congratulations Gaurav, Kaushal and Mohit!</li>
+						
+						<li>Nov '16 - Brain Segmentation paper accepted to ICVGIP 2016. Congratulations Apoorva and Gaurav!</li>
+						
+						<li>Aug '16 - Grateful to Microsoft for the unrestricted research grant.</li>
+						
 						<li>I will be teaching <a href="courses/f2016/csl603/csl603.html">CSL603 - Machine Learning</a> in Fall 2016. Check the course webpage for details.</li>
 						
 						<li>June '16 - Spot Garbage paper  accepted to UbiComp 2016! Congratulations Gaurav, Kaushal and Mohit - an all undergraduate team!</li>
@@ -92,20 +189,22 @@
 						<li>Co-authored book on <a href="http://as.wiley.com/WileyCDA/WileyTitle/productCd-111889376X.html">Activity Learning: Discovering, Recognizing, and Predicting Human Behavior from Sensor Data</a> was published by Wiley.</li>
 					</ul>
 				</div>
+				-->
 
+			<!--
 				<div class = "container_data_updates">
-					<h2><u>Open JRF Position</u> <img src="Images/new.gif" width="35" height="15" /></h2>
+					<h2><u>Open JRF Position</u> <img src="images/new1.png" width="25" height="25" /></h2>
 					There is a JRF opening in my lab for a DST sponsored project. I am looking for students technically strong in Mathematics and Computer Science interested in the data mining, machine learning, pervasive computing and related fields. Prospective students must also possess excellent programming and communication skills. Please check the link for details regarding the application process.
-				</div>
+				</div>-->
 
-				<div class = "container_data_updates">
-					<h2><u>Prospective PhD and MS Research Students</u> <img src="Images/new.gif" width="35" height="15" /></h2>
+				<div class = "container_data_updates" style = "text-align:justify">
+					<h2><u>Prospective PhD and MS Research Students</u> <img src="images/new1.png" width="25" height="25" /></h2>
 					I am looking for students technically strong in Mathematics and Computer Science interested in the data mining, machine learning, pervasive computing and related fields. Prospective students must also possess excellent programming and communication skills. Please contact me if you are one of them. I am also interested in industry/health care related problems that can be solved using data mining and machine learning techniques. Please do contact me if you have an interesting problem.
 				</div>
 
-				<div class = "container_data_updates">
-					<h2><u>Summer/Winter Internship</u> <img src="Images/new.gif" width="35" height="15" /></h2>
-					There are no openings for summer (2017) internship in my lab.
+				<div class = "container_data_updates" style = "text-align:justify">
+					<h2><u>Summer Internship</u> <img src="images/new1.png" width="25" height="25" /></h2>
+					Students technically strong in Mathematics and Computer Science with an aptitude for data mining, ubiquitous computing and related fields may apply through the INSA SRF program. I will not be taking students outside this program and will not be able to respond to your individual queries.
 				</div>
 
 
@@ -118,17 +217,8 @@
 			<div id = "footer">
 				<br>
 				<div id = "footer_hr">
-					<hr width = "75%"/>
-					<hr width = "75%" style = "margin-top:-1.45em; padding:0em"/>
-					<hr width = "75%" style = "margin-top:-1.5em; padding:0em"/>
-					<hr width = "75%" style = "margin-top:-1.5em; padding:0em"/>
-					<hr width = "75%" style = "margin-top:-1.5em; padding:0em"/>
-					<hr width = "75%" style = "margin-top:-1.5em; padding:0em"/>
-					<hr width = "75%" style = "margin-top:-1.5em; padding:0em"/>
+					<hr width = "60%" style = "transform:translateY(0.3em);"/>
 				</div>
-				
-
-
 				<div id = "footer_options">
 						<ul class = "toolbar" style = "margin:-1.4em; padding-top:0.5em;display:inline-block">
 						  <li><a href="https://www.linkedin.com/in/narayananck">LinkedIn</a></li>
@@ -137,18 +227,39 @@
 						  
 				</div>
 				<div id = "footer_hr">
-					<hr width = "75%"/>
-					<hr width = "75%" style = "margin-top:-1.55em; padding:0em"/>
-					<hr width = "75%" style = "margin-top:-1.5em; padding:0em"/>
-					<hr width = "75%" style = "margin-top:-1.5em; padding:0em"/>
-					<hr width = "75%" style = "margin-top:-1.5em; padding:0em"/>
-					<hr width = "75%" style = "margin-top:-1.5em; padding:0em"/>
-					<hr width = "75%" style = "margin-top:-1.5em; padding:0em"/>
+					<hr width = "60%" style = "transform:translateY(-0.5em);"/>
+					<div id = "tracker" style = "transform:translateY(-0.5em)">
+						<script type="text/javascript" src="http://s2.tracemyip.org/tracker/lgUrl.php?stlVar2=1327&amp;rgtype=4684NR-IPIB&amp;pidnVar2=41297&amp;prtVar2=1&amp;scvVar2=12"></script><noscript><a href="http://en.ipwebtracker.com" target="_blank"><img src="http://s2.tracemyip.org/tracker/1327/4684NR-IPIB/41297/1/12/ans/" alt="ip tracker" border="0"></a></noscript>
+
+						&nbsp;
+						<span style = "font-size:1.1em">
+							Designed by <a href = "https://github.com/ssrp/" style = "color:#888888"><b>Sai Samarth R Phaye</b></a>
+						</span>
+					</div>
 				</div>
 			</div>
  	
 
 		</div>
 	</center>
+
+
+	<script src="js/jquery-1.12.4.js"></script>
+	<script src="js/jquery.dataTables.min.js"></script>
+	<script src="js/dataTables.bootstrap.min.js"></script>
+	<script src="js/dataTables.fixedHeader.min.js"></script>
+	<script src="js/dataTables.responsive.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/responsive.bootstrap.min.js"></script>
+	<script>
+		$(document).ready(function() {
+	    var table = $('#recent_updates').DataTable( {
+	         "order": []
+
+	    } );
+	 
+	    new $.fn.dataTable.FixedHeader( table );
+		} );
+	</script>
 </body>
 </html>
